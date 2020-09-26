e33f8bd0d367223dd032f0fec3cb0df0d95fecd9
rupal-mathur1 <70269196+rupal-mathur1@users.noreply.github.com>
2020-09-26 15:17:22 +0530

    Update menu.dart

--- lib/menu.dart
+++ lib/menu.dart
@@ -2,25 +2,31 @@ import 'package:purchase_app/user.dart';
 
 final User r1 =User (
     id: 0,
-    name: 'DRINK',
+    name: 'Latest Arrivals',
     imageUrl: 'assets/images/style1.jpg'
 
 );
 
 final User r2 =User (
     id: 1,
-    name: 'PIZZA',
+    name: 'Lastest Arrival',
     imageUrl: 'assets/images/style2.jpg'
 
 );
 
 final User r3 =User (
     id: 2,
-    name: 'BURGER',
+    name: 'Discount of 20%',
     imageUrl: 'assets/images/style3.jpg'
 
 );
-
+final User r4 =User(
+  id: 4,
+  name: 'Iphones'
+  imageUrl: 'assets/images/style3.jpg'
+);
 
 
 List<User> ads = [r1,r2,r3];
+
+
