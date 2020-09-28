0c06418dee6feb49ae97ecb3b8feddda11033912
rupal-mathur1 <70269196+rupal-mathur1@users.noreply.github.com>
2020-09-29 00:57:08 +0530

    Update menu.dart

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,32 +1,33 @@
-import 'package:purchase_app/user.dart';
+import 'package:flutter_app2/user.dart';
 
 final User r1 =User (
     id: 0,
     name: 'Latest Arrivals',
-    imageUrl: 'assets/images/style1.jpg'
+    imageUrl: 'asset/images/style1.jpg'
 
 );
 
 final User r2 =User (
     id: 1,
-    name: 'Lastest Arrival',
-    imageUrl: 'assets/images/style2.jpg'
+    name: 'new Arrival',
+    imageUrl: 'asset/images/gown.jpg'
 
 );
 
 final User r3 =User (
     id: 2,
-    name: 'Discount of 20%',
-    imageUrl: 'assets/images/style3.jpg'
+    name: 'Discount 20%',
+    imageUrl: 'asset/images/style3.jpg'
 
 );
-final User r4 =User(
-  id: 4,
-  name: 'Iphones'
-  imageUrl: 'assets/images/style3.jpg'
+final User r4 =User (
+    id: 2,
+    name: 'Discount 50%',
+    imageUrl: 'asset/images/style3.jpg'
+
 );
 
 
-List<User> ads = [r1,r2,r3];
+List<User> ads = [r1,r2,r3,r4];
 
 
